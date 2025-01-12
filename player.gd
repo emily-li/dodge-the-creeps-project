21e68ea4bd771839e490ed085678c9739596d305
emily-li <emily.li@live.co.uk>
2025-01-12 15:40:51 -0800

    Complete mob and main scene integration

--- player.gd
+++ player.gd
@@ -5,6 +5,7 @@ signal hit
 @export var speed = 400 # pixels/sec
 var screen_size
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
@@ -16,6 +17,13 @@ func _process(delta: float) -> void:
 	var velocity = _get_velocity()
 	position = _get_position(position, delta, velocity)
 	_animate(velocity)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
 
 
 func _get_velocity() -> Vector2:
@@ -50,3 +58,9 @@ func _animate(velocity: Vector2) -> void:
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
