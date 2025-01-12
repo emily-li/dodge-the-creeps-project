6a78a35c2f67059cfa1373ff414c558fe02e036d
emily-li <emily.li@live.co.uk>
2025-01-12 10:55:43 -0800

    Initial commit

---
+++ player.gd
@@ -0,0 +1,52 @@
+extends Area2D
+signal hit
+
+#@export allows values to be viewed and edited on the fly in the inspector
+@export var speed = 400 # pixels/sec
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = _get_velocity()
+	position = _get_position(position, delta, velocity)
+	_animate(velocity)
+
+
+func _get_velocity() -> Vector2:
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	return velocity.normalized() * speed
+	
+	
+func _get_position(pos: Vector2, delta: float, velocity: Vector2) -> Vector2:
+	var updated_pos = pos + velocity * delta
+	return updated_pos.clamp(Vector2.ZERO, screen_size)
+
+
+func _animate(velocity: Vector2) -> void:
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		# See the note below about the following boolean assignment.
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+		
+	if velocity.length() > 0:
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
