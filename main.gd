2446f14efa53d93abac8e6ccc82dfd6da867b238
emily-li <emily.li@live.co.uk>
2025-01-12 16:12:02 -0800

    Move comments to function naming

--- main.gd
+++ main.gd
@@ -26,36 +26,45 @@ func new_game():
 	$StartTimer.start()
 
 
-func _on_mob_timer_timeout():
-	# Create a new instance of the Mob scene.
-	var mob = mob_scene.instantiate()
-
-	# Choose a random location on Path2D.
-	var mob_spawn_location = $MobPath/MobSpawnLocation
-	mob_spawn_location.progress_ratio = randf()
-
-	# Set the mob's direction perpendicular to the path direction.
-	var direction = mob_spawn_location.rotation + PI / 2
-
-	# Set the mob's position to a random location.
-	mob.position = mob_spawn_location.position
-
-	# Add some randomness to the direction.
-	direction += randf_range(-PI / 4, PI / 4)
-	mob.rotation = direction
-
-	# Choose the velocity for the mob.
-	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
-	mob.linear_velocity = velocity.rotated(direction)
-
-	# Spawn the mob by adding it to the Main scene.
-	add_child(mob)
-
-
 func _on_score_timer_timeout() -> void:
 	score += 1
 
 
-func _on_start_timer_timeout():
+func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
 	$ScoreTimer.start()
+
+
+func _on_mob_timer_timeout() -> void:
+	add_child(_get_mob())
+	
+	
+func _get_mob() -> Node:
+	var mob: RigidBody2D = mob_scene.instantiate()
+	
+	var mob_spawn_location = _get_random_mob_spawn_location()
+	mob.position = mob_spawn_location.position
+	
+	var rotation: float = _get_mob_rotation(mob_spawn_location)
+	mob.rotation = rotation
+	mob.linear_velocity = _get_mob_velocity(rotation)
+
+	return mob
+
+
+func _get_random_mob_spawn_location() -> PathFollow2D:
+	var mob_spawn_location: PathFollow2D = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+	return mob_spawn_location
+
+
+func _get_mob_rotation(mob_location: PathFollow2D) -> float:
+	# Set the mob's direction perpendicular to the path direction.
+	var rotation: float = mob_location.rotation + PI / 2
+	var randomness: float = randf_range(-PI / 4, PI / 4)
+	return rotation + randomness
+
+
+func _get_mob_velocity(direction: float) -> Vector2:
+	var velocity: Vector2 = Vector2(randf_range(150.0, 250.0), 0.0)
+	return velocity.rotated(direction)
